f8f843723447d48d7a4f670fd6bdb5e9c5f31ead
peter mikkelsen <peterphmikkelsen@gmail.com>
2022-04-20 16:36:28 +0200

    Initial Commit

---
+++ currency-converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  currency-converter
+//
+//  Created by Peter Mikkelsen on 20/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ currency-converter/currency_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  currency_converterApp.swift
+//  currency-converter
+//
+//  Created by Peter Mikkelsen on 20/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct currency_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
